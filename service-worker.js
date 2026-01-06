0a73aecff7d793b128dcb7e5c6dceda445880551
Abhay <soniabhayprakash@gmail.com>
2026-01-06 12:25:58 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,41 @@
-const CACHE_NAME = "the-chatting-v4";
+const CACHE_NAME = "the-chatting-v5"; 
+
+const ASSETS = [
+  "/chattingplatform/",
+  "/chattingplatform/index.html",
+  "/chattingplatform/style.css",
+  "/chattingplatform/script.js",
+  "/chattingplatform/manifest.json",
+  "/chattingplatform/icon-192.png",
+  "/chattingplatform/icon-512.png"
+];
 
 self.addEventListener("install", (event) => {
-  self.skipWaiting(); 
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        "/chattingplatform/",
-        "/chattingplatform/index.html",
-        "/chattingplatform/style.css?v=3",
-        "/chattingplatform/script.js?v=3",
-        "/chattingplatform/manifest.json",
-        "/chattingplatform/icon-192.png",
-        "/chattingplatform/icon-512.png"
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
+    caches.keys().then((keys) =>
+      Promise.all(
         keys.map((key) => {
           if (key !== CACHE_NAME) {
-            return caches.delete(key); 
+            return caches.delete(key);
           }
         })
-      );
-    })
+      )
+    )
   );
-  self.clients.claim(); 
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
   );
 });
